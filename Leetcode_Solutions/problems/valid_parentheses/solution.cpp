da380b71c09046f4a3dd444ca7312531874a1064
dheerajgattupalli <dgattupalli@gmail.com>
2022-12-14 19:23:56 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- Leetcode_Solutions/problems/valid_parentheses/solution.cpp
+++ Leetcode_Solutions/problems/valid_parentheses/solution.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
+        char c;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='[' || s[i]=='{' || s[i]=='(')
-                st.push(s[i]);
+            if(s[i]=='(')
+                st.push(')');
+            else if(s[i]=='{')
+                st.push('}');
+            else if(s[i]=='[')
+                st.push(']');
             else{
-                if(st.empty())
-                    return false;
-                else if(s[i] == ')' && st.top() != '(')
-                    return false;
-                else if(s[i] == '}' && st.top() != '{')
-                    return false;
-                else if(s[i] == ']' && st.top() != '[')
-                    return false;
-                else 
-                    st.pop();
+                if(st.size()==0)return false;
+                c = st.top();
+                st.pop();
+                if(c!=s[i])return false;
             }
         }
-            if(st.empty())return true;
-        return false;
+        if(st.size()!=0)return false;
+        return true;
     }
 };
