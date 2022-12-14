6c6d9fea5f0a9216174394cb59f8765db23a4f87
dheerajgattupalli <dgattupalli@gmail.com>
2022-12-14 19:56:55 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- Leetcode_Solutions/problems/valid_anagram/solution.cpp
+++ Leetcode_Solutions/problems/valid_anagram/solution.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())return false;
-        unordered_map<char,int> hash_map;
-        for(int i=0;i<s.size();i++){
-            hash_map[s[i]]++;
-            hash_map[t[i]]--;
-        }
-        for(auto it:hash_map){
-            cout<<it.first<<' '<<it.second<<endl;
-            if(it.second!=0)return false;
+        vector<int> h1(26,0),h2(26,0);
+        for(int i=0;i<s.size();i++)h1[s[i]-'a']++;
+        for(int i=0;i<t.size();i++)h2[t[i]-'a']++;
+        for(int i=0;i<26;i++){
+            if(h1[i]!=h2[i])return false;
         }
         return true;
     }
